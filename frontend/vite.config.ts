f6d0208c38103521bf11a2b30958fd2c02bb6b01
Isaac Nudelman <62861466+nuudlman@users.noreply.github.com>
2024-09-11 17:38:31 -0500

    Enable react compiler (#3)
    * Start react compiler enablement
    
    * Dep update
    
    * Add react 19 types
    
    * Enable react compiler in eslint and build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Default behavior is to run react compiler on the entire codebase
+const ReactCompilerConfig = {};
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            ["babel-plugin-react-compiler", ReactCompilerConfig],
+          ],
+        },
+      }),
+    ],
+  };
+});
