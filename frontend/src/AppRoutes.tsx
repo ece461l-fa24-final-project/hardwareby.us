05683b5c7c02fe45bc3165d39fc577b0de820a13
Drew Britt <drewbritt02@gmail.com>
2024-10-10 21:22:27 -0500

    Add MUI library, use for Login/Signup views

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,7 +1,17 @@
 import { Routes, Route, Navigate, Outlet } from "react-router-dom"
 import { useAuth } from "./contexts/AuthContext"
-import ProjectView from './views/ProjectView'
-import AuthView from './views/AuthView'
+import ProjectView from "./views/ProjectView"
+import LoginView from "./views/LoginView"
+import SignupView from "./views/SignupView"
+
+const UnauthorizedRoutes = () => {
+    const auth = useAuth();
+
+    if(auth.user)
+        return <Navigate to="/projects" replace />
+
+    return <Outlet />
+}
 
 const AuthorizedRoutes = () => {
     const auth = useAuth();
@@ -16,7 +26,10 @@ const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Navigate to="/projects" replace />}/>
-            <Route path="/login" element={<AuthView />} />
+            <Route element={<UnauthorizedRoutes />}>
+                <Route path="/login" element={<LoginView />} />
+                <Route path="/signup" element={<SignupView />} />
+            </Route>
             <Route element={<AuthorizedRoutes />}>
                 <Route path="/projects" element={<ProjectView />} />
             </Route>
