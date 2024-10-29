e0cb837a3868d2a614992acf88fdddb92a73cc0c
ash3679 <ash3679@utexas.edu>
2024-10-28 19:29:10 -0500

    project view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,25 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import LoginView from "./views/LoginView";
-import ProjectView from "./views/ProjectView";
+import { Routes, Route } from 'react-router-dom';
+import LoginView from './views/LoginView';
+import ProjectView from './views/ProjectView';
 import AuthProvider from "./contexts/Auth.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
 
 export default function App() {
-    return (
-        <>
-            <AuthProvider>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<Navigate to="/projects" replace />}
-                    />
-                    <Route path="/login" element={<LoginView />} />
-                    <Route
-                        path="/projects"
-                        element={
-                            <ProtectedRoute>
-                                <ProjectView />
-                            </ProtectedRoute>
-                        }
-                    />
-                </Routes>
-            </AuthProvider>
-        </>
-    );
-}
+  return (
+    <>
+        <AuthProvider>
+            <Routes>
+                <Route path="/login" element={<LoginView />} />
+                <Route path="/projects" element={
+                    //<ProtectedRoute>
+                        <ProjectView />
+                    //</ProtectedRoute>
+                } />
+            </Routes>
+        </AuthProvider>
+    </>
+  )
+};
+
+
+
--- frontend/src/views/ProjectView.tsx
+++ frontend/src/views/ProjectView.tsx
@@ -1,3 +1,48 @@
+
+import { useState } from 'react'
+import '../styles/ProjectView.css'
+import ProjectList from '../components/ProjectList'
+//import { Button, TextField } from '@mui/material';
+import '../styles/ProjectList.css'
+
+
+
+  
 export default function ProjectView() {
-    return <>Project Management</>;
+    const [count, setCount] = useState(0)
+  
+    return (
+      <>
+        <body>
+        <div>
+        <nav className="navbar">
+          <a href="#" className="nav-hardwarelogo">HardwareBy.Us</a>
+          <ul className="nav-links">
+            <li><a href="#">Log In</a></li>
+            <li><a href="#">Sign up</a></li>
+          </ul>
+        </nav>
+        </div>    
+
+        <div  className="createNewProj">
+        <button style={{ backgroundColor: 'rgb(255, 165, 0)', color: '#fff', margin: '0rem 15rem', padding: '0.5rem 1rem'}}>Create New Project </button>
+        </div>
+        
+        <div  className="projectListDiv">
+            <ProjectList />
+        </div>
+
+
+
+       
+        </body>
+       
+
+        
+  
+       
+      </>
+    )
 }
+
+
