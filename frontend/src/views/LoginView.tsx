aed94131a200a4cbff0643eb97dec8a2134cdf15
Isaac Nudelman <isaac.nudelman@utexas.edu>
2024-11-01 11:04:56 -0500

    Disable empty forms for now

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,22 +1,22 @@
-import UserForm from "../components/UserForm";
-import useAuth from "../hooks/Auth";
+// import UserForm from "../components/UserForm";
+// import useAuth from "../hooks/Auth";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginView() {
-    const auth = useAuth();
+    // const auth = useAuth();
     const navigate = useNavigate();
 
-    const onSubmit = async (
-        userid: string,
-        password: string,
-    ): Promise<boolean> => {
-        return false;
-    };
+    // const onSubmit = async (
+    //     userid: string,
+    //     password: string,
+    // ): Promise<boolean> => {
+    //     return false;
+    // };
 
     return (
         <>
             <h1>Login to Hardware By Us!</h1>
-            <UserForm submit={onSubmit} label="Log In" />
+            {/*<UserForm submit={onSubmit} label="Log In" />*/}
             <button onClick={() => navigate("/signup")}>Signup</button>
         </>
     );
--- frontend/src/views/SignupView.tsx
+++ frontend/src/views/SignupView.tsx
@@ -1,21 +1,21 @@
 import { useNavigate } from "react-router-dom";
-import UserForm from "../components/UserForm";
+// import UserForm from "../components/UserForm";
 
 export default function SignupView() {
     const navigate = useNavigate();
 
-    const onSubmit = async (
-        userid: string,
-        password: string,
-    ): Promise<boolean> => {
-        // TODO: if signup is successful, navigate the user to log in
-        return false;
-    };
+    // const onSubmit = async (
+    //     userid: string,
+    //     password: string,
+    // ): Promise<boolean> => {
+    //     // TODO: if signup is successful, navigate the user to log in
+    //     return false;
+    // };
 
     return (
         <>
             <h1>Signup for Hardware By Us!</h1>
-            <UserForm submit={onSubmit} label="Sign Up" />
+            {/*<UserForm submit={onSubmit} label="Sign Up" />*/}
             <button onClick={() => navigate("/login")}>Login</button>
         </>
     );
