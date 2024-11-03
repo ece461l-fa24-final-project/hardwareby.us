523edba528617382d31cf10a7be7d7354455e055
Joss B <joss.bonner@utexas.edu>
2024-11-03 11:59:13 -0600

    1. Fixed api calls 2. Fixed token to string issue, let me know if good 3. Error messages 4. And form "{}" extras

--- frontend/src/views/SignupView.tsx
+++ frontend/src/views/SignupView.tsx
@@ -5,26 +5,32 @@ export default function SignupView() {
     const navigate = useNavigate();
 
     //Signup function to handle the user registration process
-    const onSubmit = async (userId: string, password: string): Promise<boolean> => {
+    const onSubmit = async (
+        userId: string,
+        password: string,
+    ): Promise<boolean> => {
         try {
             //API call to create a new account
-            const response = await fetch(`/api/v1/auth/sign-up?userid=${encodeURIComponent(userId)}&password=${encodeURIComponent(password)}`, {
-                method: "POST",
-            });
+            const response = await fetch(
+                `/api/v1/auth/signup?userid=${encodeURIComponent(userId)}&password=${encodeURIComponent(password)}`,
+                {
+                    method: "POST",
+                },
+            );
 
             //Check if signup was successful
-            if(response.ok){
+            if (response.ok) {
                 //if so navigate the user to login page
-                navigate("/login") 
+                navigate("/login");
                 return true;
             } else {
                 // Handle error response (e.g., user already exists)
-                alert("An unexpected error occurred. Please try again later.")
+                alert("Invalid username or password. Please try again.");
                 return false; // Failed new user creation
             }
         } catch (error) {
             console.error("An error occurred during signup", error);
-            alert("An unexpected error occurred. Please try again later.")
+            alert("An unexpected error occurred. Please try again later.");
             return false;
         }
     };
@@ -32,8 +38,8 @@ export default function SignupView() {
     return (
         <>
             <h1>Signup for Hardware By Us!</h1>
-            {<UserForm submit={onSubmit} label="Sign Up" />}
-            <button onClick={() => navigate("/login")}>Login</button>
+            <UserForm submit={onSubmit} label="Sign Up" />
+            <button onClick={() => navigate("/signup")}>Login</button>
         </>
     );
 }
