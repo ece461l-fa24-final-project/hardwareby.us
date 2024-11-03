e55f335b29092a34dfc2affc18213e14fc821d82
Xander Bonner <157523819+XanderBonner@users.noreply.github.com>
2024-11-03 14:41:45 -0600

    Add signup and login views (#69)
    Co-authored-by: Joss B <joss.bonner@utexas.edu>

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,22 +1,50 @@
-// import UserForm from "../components/UserForm";
-// import useAuth from "../hooks/Auth";
+import UserForm from "../components/UserForm";
+import useAuth from "../hooks/Auth";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginView() {
-    // const auth = useAuth();
     const navigate = useNavigate();
+    const auth = useAuth(); //Access contect using useAuth
 
-    // const onSubmit = async (
-    //     userid: string,
-    //     password: string,
-    // ): Promise<boolean> => {
-    //     return false;
-    // };
+    //Signin function to handle user sign in
+    const onSubmit = async (
+        userId: string,
+        password: string,
+    ): Promise<boolean> => {
+        try {
+            // API call to authenticate the user
+            const response = await fetch(
+                `/api/v1/auth/login?userid=${encodeURIComponent(userId)}&password=${encodeURIComponent(password)}`,
+                {
+                    method: "POST",
+                },
+            );
+
+            //Check if signup was successful
+            if (response.ok) {
+                const tokenString = await response.text(); // Get token directly as a string
+                const token = { data: tokenString };
+                auth.login(token); // Pass the token string directly to auth.login
+                navigate(`/projects`); //Redirect to the projects page
+                return true;
+            } else {
+                // Handle error response (e.g., user already exists)
+                alert(
+                    "Either your Username or Password is wrong. Please try again.",
+                );
+                return false; // Failed new user creation
+            }
+        } catch (error) {
+            console.error("An error occurred during signup", error);
+            alert("An unexpected error occurred. Please try again later.");
+            return false;
+        }
+    };
 
     return (
         <>
             <h1>Login to Hardware By Us!</h1>
-            {/*<UserForm submit={onSubmit} label="Log In" />*/}
+            <UserForm submit={onSubmit} label="Log In" />
             <button onClick={() => navigate("/signup")}>Signup</button>
         </>
     );
