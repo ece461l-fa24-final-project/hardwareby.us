e145e5e93ab1e912b4ee0bbd3c1f8d2d474086df
Drew Britt <drewbritt02@gmail.com>
2024-11-07 22:31:38 -0600

    Readd App/ProjectView files that got deleted for some reason

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginView from "./views/LoginView";
+import ProjectView from "./views/ProjectView";
+import AuthProvider from "./contexts/Auth.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import SignupView from "./views/SignupView.tsx";
+
+export default function App() {
+    return (
+        <>
+            <AuthProvider>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Navigate to="/projects" replace />}
+                    />
+                    <Route path="/login" element={<LoginView />} />
+                    <Route path="/signup" element={<SignupView />} />
+                    <Route
+                        path="/projects"
+                        element={
+                            <ProtectedRoute>
+                                <ProjectView />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Routes>
+            </AuthProvider>
+        </>
+    );
+}
