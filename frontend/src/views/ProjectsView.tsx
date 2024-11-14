894d788ebb7a3a2be3476cd5e88c962f076e69ae
simonyarmo <51716669+simonyarmo@users.noreply.github.com>
2024-11-13 19:19:30 -0600

    Project View (#82)
    * New Branch, Project -> Projects
    
    Change ProjectView to ProjectsView
    
    * Hard and ware on the brain
    
    * Hard and ware on the brain
    
    * Hard and ware on the brain
    
    * Hard and ware on the brain
    
    * Need to change the return for a 404 error
    
    * changed back to HardwareSet Object
    
    * Fix linter errors + prettier
    
    * Not sure how this statement got deleted
    
    ---------
    
    Co-authored-by: Drew Britt <drewbritt02@gmail.com>

---
+++ frontend/src/views/ProjectsView.tsx
@@ -0,0 +1,22 @@
+import ProjectList from "../components/ProjectList.tsx";
+import "../styles/ProjectView.css";
+import "../styles/ProjectList.css";
+import CreateProjectDialog from "../components/CreateProjectDialog.tsx";
+import { Token } from "../contexts/Auth";
+
+interface ProjectsViewProps {
+    token: Token;
+}
+
+export default function ProjectsView({ token }: Readonly<ProjectsViewProps>) {
+    return (
+        <div className="container">
+            <div className="project-list-div">
+                <ProjectList token={token} />
+            </div>
+            <div className="create-new-proj">
+                <CreateProjectDialog token={token} />
+            </div>
+        </div>
+    );
+}
