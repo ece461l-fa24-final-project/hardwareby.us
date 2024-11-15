d05d87375cdea03ee14f3c7c9ef371d15d6f5b42
Drew Britt <drewbritt02@gmail.com>
2024-11-14 19:28:29 -0600

    Navabar + Logout button (#92)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar.tsx";
 import LoginView from "./views/LoginView.tsx";
 import ProjectsView from "./views/ProjectsView.tsx";
 import AuthProvider from "./contexts/Auth.tsx";
@@ -9,6 +10,7 @@ export default function App() {
     return (
         <>
             <AuthProvider>
+                <Navbar />
                 <Routes>
                     <Route
                         path="/"
